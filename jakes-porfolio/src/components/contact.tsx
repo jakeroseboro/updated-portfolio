2bd65076a6e47e01790385d28f8e80780afe46c7
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2022-02-05 13:16:18 -0600

    work on contact form

--- jakes-porfolio/src/components/contact.tsx
+++ jakes-porfolio/src/components/contact.tsx
@@ -2,13 +2,23 @@ import React from "react";
 import Fade from "react-reveal/Fade";
 import { Container, Row, Col } from "react-bootstrap";
 import emailjs from "emailjs-com";
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, message } from "antd";
 
 export const Skills = () => {
   const handleSubmit = (e: any) => {
-    e.preventDefault();
+    
     console.log(e);
-    //emailjs.sendForm('jakeroseboro', 'jakeroseboro', )
+    emailjs
+      .send("jakeroseboro", "jakeroseboro", {
+        message_email: e.message_email,
+        message: e.message,
+        from_first_name: e.from_first_name,
+        from_last_name: e.from_last_name,
+      }, "user_vbjYlIJevYSxTjDOUmDQ0")
+      .then((_) => {
+        return message.success("email sent");
+      })
+      .catch((e) => console.log(e));
   };
 
   return (
@@ -20,24 +30,34 @@ export const Skills = () => {
         <Fade bottom duration={600} delay={100} distance="30px">
           <div className="contact-wrapper">
             <Form name="nest-messages" onFinish={handleSubmit}>
+              <Row>
+                <Col>
+                  <Form.Item
+                    name="from_first_name"
+                    label="First Name"
+                    rules={[{ required: true }]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col>
+                  <Form.Item
+                    name="from_last_name"
+                    label="Last Name"
+                    rules={[{ required: true }]}
+                  >
+                    <Input />
+                  </Form.Item>
+                </Col>
+              </Row>
               <Form.Item
-                name={["user", "name"]}
-                label="Name"
-                rules={[{ required: true }]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                name={["user", "email"]}
+                name="message_email"
                 label="Email"
                 rules={[{ type: "email" }]}
               >
                 <Input />
               </Form.Item>
-              <Form.Item name={["user", "website"]} label="Website">
-                <Input />
-              </Form.Item>
-              <Form.Item name={["user", "introduction"]} label="Introduction">
+              <Form.Item name="message" label="Message">
                 <Input.TextArea />
               </Form.Item>
               <Form.Item>
